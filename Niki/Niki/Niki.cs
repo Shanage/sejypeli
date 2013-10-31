4d4d378a806dfefba6c506bf70a7063b4318b52d
Shanage <sanna.sahlgren@gmail.com>
2013-10-31 09:28:28 -0700

    Oma peli

---
+++ Niki/Niki/Niki.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class Niki : PhysicsGame
+{
+    const double nopeus = 200;
+    const double hyppyNopeus = 750;
+    const int RUUDUN_KOKO = 40;
+
+    PlatformCharacter pelaaja1;
+
+    Image pelaajanKuva = LoadImage("hahmopixeli");
+    Image marjanKuva = LoadImage("marja");
+
+    public override void Begin()
+    {
+        Gravity = new Vector(0, -1000);
+
+        LuoKentta();
+        LisaaNappaimet();
+
+        Camera.Follow(pelaaja1);
+        Camera.ZoomFactor = 1.2;
+        Camera.StayInLevel = true;
+    }
+
+    void LuoKentta()
+    {
+        ColorTileMap ruudut = ColorTileMap.FromLevelAsset("kenttaNiki2");
+
+        ruudut.SetTileMethod(Color.FromHexCode("2629FF"), LisaaPelaaja);
+        ruudut.SetTileMethod(Color.FromHexCode("562F20"), LisaaTaso);
+        ruudut.SetTileMethod(Color.FromHexCode("FF0004"), LisaaMarja);
+
+        ruudut.Execute();
+    }
+
+    void LisaaTaso(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        taso.Position = paikka;
+        taso.Color = Color.Green;
+        Add(taso);
+    }
+
+    void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
+    {
+        pelaaja1 = new PlatformCharacter(leveys, korkeus);
+        pelaaja1.Position = paikka;
+        pelaaja1.Mass = 4.0;
+        pelaaja1.Image = pelaajanKuva;
+        Add(pelaaja1);
+    }
+
+    void LisaaMarja(Vector paikka, double leveys, double korkeus)
+    {
+        IPhysicsObject marja = new PhysicsObject(5, 5);
+        marja.IgnoresCollisionResponse = true;
+        marja.Position = paikka;
+        marja.Image = marjanKuva;
+        marja.Tag = "marja";
+        Add(marja, 1);
+
+    }
+
+    void LisaaNappaimet()
+    {
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -nopeus);
+        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, nopeus);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, hyppyNopeus);
+
+        ControllerOne.Listen(Button.Back, ButtonState.Pressed, Exit, "Poistu pelistä");
+
+        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1, -nopeus);
+        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, nopeus);
+        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, hyppyNopeus);
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+    }
+
+    void Liikuta(PlatformCharacter hahmo, double nopeus)
+    {
+        hahmo.Walk(nopeus);
+    }
+
+    void Hyppaa(PlatformCharacter hahmo, double nopeus)
+    {
+        hahmo.Jump(nopeus);
+    }
+
+}
